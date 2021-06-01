2937c257daa0eab5377e094e8daa9346b26125ea
linl <linl@redhat.com>
2021-06-01 20:16:53 +0800

    Add pydocs for LTP cases.

--- tests/aws/test_functional_ltp.py
+++ tests/aws/test_functional_ltp.py
@@ -19,8 +19,26 @@ class LTPRun(Test):
     def test_ltp_hugemmap(self):
         '''
         :avocado: tags=test_ltp_hugemmap
-        polarion_id: RHEL7-88729
-        BZ#: 1312331
+        description:
+            os-tests Test ltp hugemmap case in RHEL on AWS. Linked case RHEL7-88729.
+        polarion_id:
+            https://polarion.engineering.redhat.com/polarion/#/project/RedHatEnterpriseLinux7/workitems?query=title:"[AWS]LTPRun.test_ltp_hugemmap"
+        bugzilla_id: 
+            1312331
+        customer_case_id: 
+            n/a
+        maintainer: 
+            xiliang
+        case_priority: 
+            0
+        case_component: 
+            LTP
+        key_steps:
+            1. Launch an instance on AWS.
+            2. Download and install ltp tool from https://github.com/linux-test-project/ltp or download the built rpm package from 
+            3. Run hugemmap test with command "$ sudo /opt/ltp/runltp -s hugemmap".
+        pass_criteria: 
+            System doesn't crash, panic or hang, and tests pass.
         '''
         case_name = "os_tests.tests.test_ltp.TestLTP.test_ltp_hugemmap"
         utils_lib.run_os_tests(self, case_name=case_name)
@@ -28,8 +46,27 @@ class LTPRun(Test):
     def test_ltp_cpuhotplug(self):
         '''
         :avocado: tags=test_ltp_cpuhotplug
-        polarion_id: RHEL7-98752
-        BZ#: 1464095
+        description:
+            os-tests Test ltp cpuhotplug case in RHEL on AWS. Linked case RHEL7-98752.
+        polarion_id:
+            https://polarion.engineering.redhat.com/polarion/#/project/RedHatEnterpriseLinux7/workitems?query=title:"[AWS]LTPRun.test_ltp_cpuhotplug"
+        bugzilla_id: 
+            1464095
+        customer_case_id: 
+            n/a
+        maintainer: 
+            xiliang
+        case_priority: 
+            0
+        case_component: 
+            LTP
+        key_steps:
+            1. Launch an instance on AWS.
+            2. Download and install ltp tool from https://github.com/linux-test-project/ltp or download the built rpm package from 
+            3. Run ltp cpuhotplug test with command "$ sudo /opt/ltp/runltp -f cpuhotplug".
+            4. Also can manual online/offline cpu via command "echo 0 > /sys/devices/system/cpu/cpuN/online".
+        pass_criteria: 
+            System doesn't crash, panic or hang, and tests pass.
         '''
         # ltp will considering fail if more than 1 cpus cannot be offline
         # in bare metal instance
@@ -41,8 +78,27 @@ class LTPRun(Test):
     def test_ltp_add_key02(self):
         '''
         :avocado: tags=test_ltp_add_key02
-        polarion_id: RHEL7-98753
-        BZ#: 1464851
+        description:
+            os-tests Test ltp cpuhotplug case in RHEL on AWS. Linked case RHEL7-98753.
+        polarion_id:
+            https://polarion.engineering.redhat.com/polarion/#/project/RedHatEnterpriseLinux7/workitems?query=title:"[AWS]LTPRun.test_ltp_add_key02"
+        bugzilla_id: 
+            1464851
+        customer_case_id: 
+            n/a
+        maintainer: 
+            xiliang
+        case_priority: 
+            0
+        case_component: 
+            LTP
+        key_steps:
+            1. Launch an instance on AWS.
+            2. Download and install ltp tool from https://github.com/linux-test-project/ltp or download the built rpm package from 
+            3. Run ltp syscalls test with command "$ sudo /opt/ltp/runltp -f syscalls".
+            4. Run test "$ sudo /opt/ltp/runltp -f syscalls -s add_key02".
+        pass_criteria: 
+            System doesn't crash, panic or hang, and all tests pass.
         '''
         case_name = "os_tests.tests.test_ltp.TestLTP.test_ltp_add_key02"
         utils_lib.run_os_tests(self, case_name=case_name)
@@ -50,7 +106,26 @@ class LTPRun(Test):
     def test_ltp_quickhit(self):
         '''
         :avocado: tags=test_ltp_quickhit
+        description:
+            os-tests Test ltp quickhit case for RHEL on AWS.
         polarion_id:
+            https://polarion.engineering.redhat.com/polarion/#/project/RedHatEnterpriseLinux7/workitems?query=title:"[AWS]LTPRun.test_ltp_quickhit"
+        bugzilla_id: 
+            n/a
+        customer_case_id: 
+            n/a
+        maintainer: 
+            xiliang
+        case_priority: 
+            0
+        case_component: 
+            LTP
+        key_steps:
+            1. Launch an instance on AWS.
+            2. Download and install ltp tool from https://github.com/linux-test-project/ltp or download the built rpm package from 
+            3. Run ltp quickhit test with command "$ sudo /opt/ltp/runltp -f quickhit".
+        pass_criteria: 
+            System doesn't crash, panic or hang, and tests pass.
         '''
         case_name = "os_tests.tests.test_ltp.TestLTP.test_ltp_quickhit"
         utils_lib.run_os_tests(self, case_name=case_name)
@@ -58,7 +133,28 @@ class LTPRun(Test):
     def test_ltp_ipsec_icmp(self):
         '''
         :avocado: tags=test_ltp_ipsec_icmp
-        polarion_id: RHEL7-98754
+        description:
+            os-tests Test ltp ipsec_icmp case in RHEL on AWS. Linked case RHEL7-98754.
+        polarion_id:
+            https://polarion.engineering.redhat.com/polarion/#/project/RedHatEnterpriseLinux7/workitems?query=title:"[AWS]LTPRun.test_ltp_ipsec_icmp"
+        bugzilla_id: 
+            1473593
+        customer_case_id: 
+            n/a
+        maintainer: 
+            xiliang
+        case_priority:
+            0
+        case_component: 
+            LTP
+        key_steps:
+            1. Launch an instance on AWS.
+            2. Download and install ltp tool from https://github.com/linux-test-project/ltp or download the built rpm package from 
+            3. Run ltp ipsec_icmp test with command "$ sudo /opt/ltp/runltp -f net_stress.ipsec_icmp -s icmp4-uni-vti11".
+            4. Check if ccm module is loaded via command "lsmod | grep ccm".
+            5. Remove ccm module via command "modprobe -r ccm".
+        pass_criteria: 
+            System doesn't crash, panic or hang, and tests pass.
         '''
         case_name = "os_tests.tests.test_ltp.TestLTP.test_ltp_ipsec_icmp"
         utils_lib.run_os_tests(self, case_name=case_name)
